cd75617cea7937ef8c487b2ea44a4e6d567d09c2
kelompokptik <cyberlast173@gmail.com>
2024-04-19 22:50:57 +0700

    TicTacToe

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ squares, xIsNext, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = [...squares];
+    nextSquares[i] = xIsNext ? "X" : "O";
+
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+
+  const status = winner
+    ? "Winner : " + winner
+    : "Next Player : " + (xIsNext ? "X" : "O");
+  return (
+    <>
+      <div className="info">{status}</div>
+      <div className="board">
+        {squares.map((square, index) => {
+          return (
+            <Square
+              key={index}
+              onSquareClick={() => handleClick(index)}
+              value={square}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default function game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXisNext] = useState(true);
+  const [curentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[curentMove];
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXisNext(move % 2 === 0);
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, curentMove + 1), nextSquares];
+    setCurrentMove(nextHistory.length - 1);
+    setHistory(nextHistory);
+    setXisNext(!xIsNext);
+  }
+
+  const moves = history.map((squares, move) => {
+    const description = move > 0 ? "Go to Move " + move : "Go to Game Start";
+
+    return (
+      <li key={move}>
+        <button key={move} onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={currentSquares}
+            xIsNext={xIsNext}
+            onPlay={handlePlay}
+          />
+        </div>
+
+        <div className="time-travel">
+          <ul>{moves}</ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function calculateWinner(squares) {
+  const lines = [
+    // Garis horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Garis vertikal
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Garis diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const a = lines[i][0];
+    const b = lines[i][1];
+    const c = lines[i][2];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return false;
+}
